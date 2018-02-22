1baf3fbf13131631337014b0a063158121c469cf
David Lamouller <dlamouller@5510-cb3tmc2.gopro.lcl>
2018-02-22 21:15:05 +0100

    remove class bin, hex, oct

--- binaryrepr.py
+++ binaryrepr.py
@@ -4,7 +4,7 @@
 """
 File: binaryrepr.py
 Author: David LAMOULLER
-Email: yourname@email.com
+Email: dlamouller@protonmail.com
 Github: https://github.com/yourname
 Description: 
 """
@@ -12,8 +12,7 @@ Description:
 from __future__ import unicode_literals
 import sys
 from math import log
-from jinja2 import Template
-import  click
+import click
 
 #option fo click
 click.disable_unicode_literals_warning = True
@@ -25,14 +24,26 @@ delim = lambda x, y: " " * x + y + " "
 class BaseDepiction(object):
     """BaseDepiction"""
 
-    def __init__(self, x, outformat="basic", short_repr=False, verbosity=False):
+    def __init__(self, x, type_rep="bin", outformat="basic", short_repr=False, verbosity=False):
         super(BaseDepiction, self).__init__()
-        self.x = x
+        try:
+            if x.startswith('0x'):
+                self.x = int(x, 16)
+            elif x.startswith('0b'):
+                self.x = int(x, 2)
+            elif x.startswith('0'):
+                self.x = int(x, 8)
+            else:
+                self.x = int(x)
+        except ValueError as e:
+            print("!!! Oups !!! {}".format(e))
+            sys.exit()
+        self.power = dict(hex=4, oct=3).get(type_rep, 1)
         self.outformat = outformat
-        self.delimiter = "|"
-        self.delimiterhead = "--|"
         self.short_repr = short_repr
         self.verbosity = verbosity
+        self.delimiter = "|"
+        self.delimiterhead = "--|"
         if self.verbosity:
             self.reprpos = "position bits "
             self.reprbin = "value "
@@ -40,14 +51,29 @@ class BaseDepiction(object):
             self.reprpos = ""
             self.reprbin = ""
         self.depth = 7
-        for d in filter(lambda e: round(log(x, 2) + 0.5) <= e, [8, 16, 32, 64, 128]):
+        for d in filter(lambda e: round(log(self.x, 2) + 0.5) <= e, [8, 16, 32, 64, 128]):
             self.depth = d
             break
-        self.position = list(map(str, range(self.depth, -1, -1)))
+        if self.power == 4:
+            self.x = format(self.x, 'x')
+        elif self.power == 3:
+            self.x = format(self.x, 'o')
+        else:
+            self.x = format(self.x, 'b')
+
+        if not self.short_repr:
+            self.x = int((self.depth/self.power - len(self.x) + 1)) * "0" + self.x
+        if sys.byteorder == "little":
+            nbbits = [i * self.power for i in range(len(self.x) -1, -1, -1)]
+        else:
+            nbbits = [i * self.power for i in range(len(self.x) -1)]
+        self.position = list(map(str, nbbits))
+
 
     def __repr__(self):
         header = ""
         data   = ""
+
         if self.verbosity:
             print("endianness: {0}".format(sys.byteorder))
         adjustspace = len(self.reprpos) - len(self.reprbin)
@@ -64,7 +90,6 @@ class BaseDepiction(object):
                     delimheader = delimheader.replace("---|", ":--|")
                 elif self.outformat == "right":
                     delimheader = delimheader.replace("---|", "--:|")
-
             else:
                 delimheader += self.delimiterhead
         delimheader = "|" + delimheader + "\n"
@@ -88,74 +113,21 @@ class BaseDepiction(object):
         data = self.reprbin + delim(adjustspace, self.delimiter) + data[:-1] + "\n"
         return header + delimheader + data
 
-class Bin(BaseDepiction):
-    """Binary representation"""
-
-    def __init__(self, x, outformat="basic", short_repr=False, verbosity=False):
-        super(BaseDepiction, self).__init__()
-        BaseDepiction.__init__(self, x, outformat, short_repr, verbosity)
-        self.x = format(x, 'b')
-        if short_repr:
-            if sys.byteorder == "little":
-                self.position = list(map(str, range(len(self.x) - 1, -1, -1)))
-            else:
-                self.position = list(map(str, range(len(self.x) - 1)))
-        else:
-            self.x = (self.depth - len(self.x) + 1) * "0" + self.x
-
-
-class Hex(BaseDepiction):
-    """Hexadecimal representation"""
-    
-    def __init__(self, x, outformat="basic", short_repr=False, verbosity=True):
-        super(BaseDepiction, self).__init__()
-        BaseDepiction.__init__(self, x, outformat, verbosity=verbosity)
-        self.x = format(x, 'x')
-        if not short_repr:
-            self.x = int((self.depth/4 - len(self.x) + 1)) * "0" + self.x
-        if sys.byteorder == "little":
-            nbbits = [i * 4 for i in range(len(self.x) - 1, -1, -1)]
-        else:
-            nbbits = [i * 4 for i in range(len(self.x) - 1)]
-        self.position = list(map(str, nbbits))
-
-    
-class Oct(BaseDepiction):
-    """Oct representation"""
-
-    def __init__(self, x, outformat ="basic", short_repr=False, verbosity=True):
-        super(BaseDepiction, self).__init__()
-        BaseDepiction.__init__(self, x, outformat, verbosity=verbosity)
-        self.x = format(x, 'o')
-        if not short_repr:
-            self.x = int((self.depth/2 - len(self.x) + 1)) * "0" + self.x
-        if sys.byteorder == "little":
-            nbbits = [i * 3 for i in range(len(self.x) -1, -1, -1)]
-        else:
-            nbbits = [i * 3 for i in range(len(self.x) -1)]
-        self.position = list(map(str, nbbits))
 
 @click.command(context_settings=CONTEXT_SETTINGS, help="representation of a number in binary, hexadecimal or oct according to your system byteorder")
-@click.option("-t", "--type_repr", default="bin", type=click.Choice(['bin', 'hex', 'oct']), help="type of representation of number")
+@click.option("-t", "--type_repr", default="bin", type=click.Choice(['bin', 'hex', 'oct']), help="type of representation of number, binary by default")
 @click.option("-f", "--outformat", default="basic", type=click.Choice(['center', 'left', 'right', 'basic']), help="outpout format representation. basic by default")
 @click.option("-s", "--short", is_flag=True, help="short representation")
 @click.option("-v", "--verbose", is_flag=True, help="verbose mode")
-@click.argument("value", type=int)
+@click.argument("value")
 def binaryrepr(value, type_repr, outformat, short, verbose):
     if verbose:
-        print("representation of {0} in {1}".format(value, type_repr))
-    if type_repr=="hex" or type_repr=="h":
-        print(Hex(value, outformat, short_repr=short, verbosity=verbose))
-    elif type_repr=="oct" or type_repr=="o":
-        print(Oct(value, outformat, short_repr=short, verbosity=verbose))
+        print("representation of {0}".format(value))
+        print("{0}".format(BaseDepiction(value, type_repr, outformat, short, verbose)))
     else:
-        print("{0}".format(Bin(value, outformat, short, verbose)))
+        print("{0}".format(BaseDepiction(value, type_repr, outformat, short, verbose)))
 
 
 if __name__ == "__main__":
     binaryrepr()
-    # value = 2
-    # print("representation de {0}:\n{1}".format(value, Bin(value, verbosity=True)))
-    # print(Hex(value, verbosity=False))
-    # print(Oct(value, verbosity=False))
 
