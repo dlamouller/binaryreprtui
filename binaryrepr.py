b58db492d07e68d114f6bd55c67856980b17c929
david Lamouller <dlamouller@protonmail.com>
2020-11-19 23:31:14 +0100

    better print

--- binaryrepr.py
+++ binaryrepr.py
@@ -40,64 +40,64 @@ class BaseDepiction(object):
     """BaseDepiction
 
     >>> BaseDepiction((0, u'd'), True, "bin", "basic", False, True)
-    +-------+--------+--------+---+---+---+---+---+---+---+---+---+
-    | input | ffs_u8 | nlz_u8 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
-    +-------+--------+--------+---+---+---+---+---+---+---+---+---+
+    +-------+--------+--------+---+---+---+---+---+---+---+---+
+    | input | ffs_u8 | nlz_u8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    +-------+--------+--------+---+---+---+---+---+---+---+---+
 
     >>> BaseDepiction((182, u'd'), True, "bin", "basic", False, True)
     +-------+--------+--------+---+---+---+---+---+---+---+---+
-    | input | ffs_u7 | nlz_u7 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | input | ffs_u8 | nlz_u8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
     +-------+--------+--------+---+---+---+---+---+---+---+---+
 
     >>> BaseDepiction((256, u'd'), True, "bin", "basic", False, True)
     +-------+---------+---------+----+----+----+----+----+----+---+---+---+---+---+---+---+---+---+---+
-    | input | ffs_u15 | nlz_u15 | 15 | 14 | 13 | 12 | 11 | 10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | input | ffs_u16 | nlz_u16 | 15 | 14 | 13 | 12 | 11 | 10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
     +-------+---------+---------+----+----+----+----+----+----+---+---+---+---+---+---+---+---+---+---+
 
     >>> BaseDepiction((2, u'd'), True, "bin", "gfm", False, True)
-    | input | ffs_u7 | nlz_u7 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | input | ffs_u8 | nlz_u8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 
     >>> BaseDepiction((2, u'd'), True, "bin", "nohrules", False, True)
-    | input | ffs_u7 | nlz_u7 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | input | ffs_u8 | nlz_u8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 
     >>> BaseDepiction((32, u'd'), True, "bin", "basic", True, True)
     +-------+--------+--------+---+---+---+---+---+---+
-    | input | ffs_u7 | nlz_u7 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | input | ffs_u8 | nlz_u8 | 5 | 4 | 3 | 2 | 1 | 0 |
     +-------+--------+--------+---+---+---+---+---+---+
 
     >>> BaseDepiction((1, u'x'), False, "bin", "basic", False, True)
     +-----------+-------+--------+--------+---+---+---+---+---+---+---+---+
-    | value dec | input | ffs_u7 | nlz_u7 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | value dec | input | ffs_u8 | nlz_u8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
     +-----------+-------+--------+--------+---+---+---+---+---+---+---+---+
 
     >>> BaseDepiction((15, u'x'), False, "bin", "basic", True, True)
     +-----------+-------+--------+--------+---+---+---+---+
-    | value dec | input | ffs_u7 | nlz_u7 | 3 | 2 | 1 | 0 |
+    | value dec | input | ffs_u8 | nlz_u8 | 3 | 2 | 1 | 0 |
     +-----------+-------+--------+--------+---+---+---+---+
 
     >>> BaseDepiction((15, u'x'), False, "hex", "basic", False, True)
     +-----------+-------+--------+--------+---+---+
-    | value dec | input | ffs_u7 | nlz_u7 | 4 | 0 |
+    | value dec | input | ffs_u8 | nlz_u8 | 4 | 0 |
     +-----------+-------+--------+--------+---+---+
 
     >>> BaseDepiction((15, u'b'), False, "bin", "basic", True, True)
     +-----------+-------+--------+--------+---+---+---+---+
-    | value dec | input | ffs_u7 | nlz_u7 | 3 | 2 | 1 | 0 |
+    | value dec | input | ffs_u8 | nlz_u8 | 3 | 2 | 1 | 0 |
     +-----------+-------+--------+--------+---+---+---+---+
 
     >>> BaseDepiction((31, u'b'), False, "hex", "basic", True, True)
     +-----------+-------+--------+--------+---+---+
-    | value dec | input | ffs_u7 | nlz_u7 | 4 | 0 |
+    | value dec | input | ffs_u8 | nlz_u8 | 4 | 0 |
     +-----------+-------+--------+--------+---+---+
 
     >>> BaseDepiction((31, u'b'), False, "oct", "basic", True, True)
     +-----------+-------+--------+--------+---+---+
-    | value dec | input | ffs_u7 | nlz_u7 | 3 | 0 |
+    | value dec | input | ffs_u8 | nlz_u8 | 3 | 0 |
     +-----------+-------+--------+--------+---+---+
 
     >>> BaseDepiction((8, u'd'), True, "oct", "basic", False, True)
     +-------+--------+--------+---+---+---+
-    | input | ffs_u7 | nlz_u7 | 6 | 3 | 0 |
+    | input | ffs_u8 | nlz_u8 | 6 | 3 | 0 |
     +-------+--------+--------+---+---+---+
 
     """
@@ -123,10 +123,10 @@ class BaseDepiction(object):
                               "hrules": pt.NONE}
         if self.x:
             for deep in filter(lambda e: int(log(self.x, 2)) < e, [8, 16, 32, 64, 128]):
-                self.depth = deep -1
+                self.depth = deep - 1
                 break
         else:
-            self.depth = 8
+            self.depth = 7
         if self.power == 4:
             self.x = format(self.x, 'x')
         elif self.power == 3:
@@ -142,14 +142,13 @@ class BaseDepiction(object):
             nbbits = [i * self.power for i in range(len(self.x))]
         self.position = list(map(str, nbbits))
         self.x = list(self.x)
-        # nlz = self.depth - int(log(x[0], 2)) - 1 if x[0] else 8
         nlz = self.depth - int(log(x[0], 2)) if x[0] else 8
         ffs = self.depth - nlz
         # self.position.insert(0, "binarygap")
         # self.x.insert(0, binarygap(x[0]))
-        self.position.insert(0, "nlz_u" + str(self.depth))
+        self.position.insert(0, "nlz_u" + str(self.depth + 1))
         self.x.insert(0, nlz)
-        self.position.insert(0, "ffs_u" + str(self.depth))
+        self.position.insert(0, "ffs_u" + str(self.depth + 1))
         self.x.insert(0, ffs)
         self.position.insert(0, "input")
         self.x.insert(0, self.base + format(x[0], self.base))
@@ -160,7 +159,7 @@ class BaseDepiction(object):
 
     def __repr__(self):
         sout = self.table.get_string()
-        if self.outformat == "gfm": #don't know how to set for gfm format
+        if self.outformat == "gfm":  # don't know how to set for gfm format
             sout = sout.split("\n")
             sout = "\n".join(sout[1:-1])
         return sout
@@ -168,16 +167,15 @@ class BaseDepiction(object):
     def __add__(self, other):
         if other.depth != self.depth:
             pad = len(self.x) - len(other.x)
-            other.x[2] += (self.depth - other.depth) # update value of nlz
+            other.x[2] += (self.depth - other.depth)  # update value of nlz
             while pad > 0:
-                other.x.insert(4, "0") #first column contains values, 2nd nlz
+                other.x.insert(4, "0")  # first column contains values, 2nd nlz
                 pad -= 1
         self.table.add_row(other.x)
         return self.table.get_string()
 
     def add_row(self):
-        """add a new row
-        """
+        """add a new row"""
         self.table.add_row(self.x)
 
 
@@ -197,10 +195,26 @@ class BaseDepiction(object):
 def binaryrepr(value, type_repr, outformat, short):
     """
     display number in binary, hexadecimal or oct in a human readable view
-    
     """
-    convert = lambda x: (eval(x), 'x') if x.startswith('0x') else ((eval(x), 'b')\
-            if x.startswith('0b') else ((eval(x), 'o') if x.startswith('0') else (eval(x), 'd')))
+    def convert(x):
+        """
+        >>> convert('0x10')
+        (16, 'x')
+        >>> convert('0b10')
+        (3, 'x')
+        >>> convert('0o7')
+        (7, 'o')
+        >>> convert('10')
+        (9, 'd')
+        """
+        if x.startswith('0x'):
+            return (eval(x), 'x')
+        elif x.startswith('0b'):
+            return (eval(x), 'b')
+        elif x.startswith('0o'):
+            return (eval(x), 'o')
+        else:
+            return (eval(x), 'd')
     values = list(map(convert, value))
     alldecimal = all(y == 'd' for x, y in values)
     maxv = sorted(values, key=itemgetter(0)).pop(-1)
@@ -214,6 +228,7 @@ def binaryrepr(value, type_repr, outformat, short):
         base = BaseDepiction(val, alldecimal, type_repr, outformat, short, header=False)
         master + base
     print(master)
+
 
 if __name__ == "__main__":
     binaryrepr()
